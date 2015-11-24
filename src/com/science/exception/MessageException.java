d0e20958786fc5cce0eac0851133f603db648136
sqliang <1053196988@qq.com>
2015-11-24 16:54:51 +0800

    sqliang

--- src/com/science/exception/MessageException.java
+++ src/com/science/exception/MessageException.java
@@ -6,7 +6,7 @@ public class MessageException extends RuntimeException {
 	private boolean isShow = true;
 	public MessageException(String msg) {
 		super(msg);
-	}
+	}   
 	public String getMsg(){
 		return isShow?getMessage():"";
 	}
